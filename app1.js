60dfc643e44ee56981f1ff0a058bf0d5a577fa3b
unknown <sirico.1810153@studenti.uniroma1.it>
2020-08-27 00:28:23 +0200

    ok

--- app1.js
+++ app1.js
@@ -1,18 +1,23 @@
 var server=require('./server')
 
-/* Initialize and add the map
+ //Initialize and add the map
 function initMap() {
     var emma = {lat: 41.894798, lng: 12.4751301};
 
     var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: emma});
+    //var e=new google.maps.Marker({position:emma, map:map});
+
+    var prova=[0,1,2,3,5,6];
 
     var marker = [];
 
     for(var i=0;i<10;i++){
-        marker[i]=new google.maps.Marker({position:{lat:41.8+i, lng:12.4+i}, map:map});
+        //marker[i]=new google.maps.Marker({position:{lat:41.8+i, lng:12.4+i}, map:map});
     }
+    prova.forEach((x,i) =>  new google.maps.Marker({position: {lat: 42+x, lng: 11+x} , map:map}));
+  
   }
-  */
+    
 
   /* for(var i=0;i<10;i++){
                         marker[i]=new google.maps.Marker({position:{lat: {info.results[i].geometry.location.lat}, lng: {info.results[i].geometry.location.lng}, map:map});
@@ -20,7 +25,7 @@ function initMap() {
                     }*/
 
 
-
+/*
                 function initMap() {
                     var emma = {lat: 41.894798, lng: 12.4751301};
                     var first=emma;//{lat: coord[0].lat, lng: coord[0].lng};
@@ -37,4 +42,5 @@ function initMap() {
                        //marker[i]=new google.maps.Marker({position: coord[i].lat , lng: coord[i].lng, map:map});
                   }
                 }
+                */
                   
--- server.js
+++ server.js
@@ -123,16 +123,20 @@ app.get('/:luoghi/:citta',function(req,res){
                 coord[i]= {lat: info.results[i].geometry.location.lat, lng: info.results[i].geometry.location.lng};
                 attrazioni+='<b>'+info.results[i].name+'</b>'+' in '+info.results[i].formatted_address+'</br>';
             }
-
-            var j=0;
+            
+            var prova=[0,1,2,4];
+            //coord.forEach((x,i) =>console.log('indice: ',i,x));
+            //console.log(coord);
+            
 
             send.invia(attrazioni);
+
 
             attrazioni+=`<!DOCTYPE html>
             <html>
               <head>
                 <style>
-                  /* Set the size of the div element that contains the map */
+                   /* Set the size of the div element that contains the map */
                   #map {
                     height: 400px;  /* The height is 400 pixels */
                     width: 100%;  /* The width is the width of the web page */
@@ -140,28 +144,39 @@ app.get('/:luoghi/:citta',function(req,res){
                 </style>
               </head>
               <body>
-                <h3>Google Maps</h3>
+                <h3>My Google Maps Demo</h3>
+                <!--The div element for the map -->
                 <div id="map"></div>
+                <!--Load the API from the specified URL
+                * The async attribute allows the browser to render the page while the API loads
+                * The key parameter will contain your own API key (which is not needed for this tutorial)
+                * The callback parameter executes the initMap() function
+                -->
+                <!--<script type="text/javascript" src="./app1.js"></script>-->
                 <script>
-                function initMap() {
-                    //var emma = {lat: 41.894798, lng: 12.4751301};
-                    var first={lat: ${coord[0].lat}, lng: ${coord[0].lng}};
+                  function initMap() {
+                //var emma = {lat: 41.894798, lng: 12.4751301};
+                var first={lat: ${coord[0].lat}, lng: ${coord[0].lng}}
+            
+                var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: first});
+                //var m=new google.maps.Marker({position:first, map:map});
 
-                    var map = new google.maps.Map(document.getElementById('map'), {zoom: 10, center: first});
-                    
-                    var marker = [];
-                    for(var i=0;i<tot;i++){
-                       //marker[i]=new google.maps.Marker({position: {lat: , lng:  , map:map});
-                    }
+                var prova= [0,1,2,3];
+
+                var marker = [];
+
+                for(var i=0;i<10;i++){
+                    //marker[i]=new google.maps.Marker({position:{lat:41.8+i, lng:12.4+i}, map:map});
                 }
+                //{coord}.forEach((x,i) => new google.maps.Marker({position: {lat: x.lat, lng: x.lng} , map:map}));
+              }
                 </script>
                 <script defer
-                src="https://maps.googleapis.com/maps/api/js?key=`+process.env.GOOGLE_KEY+`+&callback=initMap">
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfoU_FqP-lC5nKYNR2qzNDynKs1TI3NuA&callback=initMap">
                 </script>
               </body>
             </html>`;
 
-            
 
             //res.send(coord);
             res.send(attrazioni);
