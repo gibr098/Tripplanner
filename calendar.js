a7b5f7309039bccbbf321381a8665b3718500b82
giuseppe gabriele sirico <sirico.1810153@studenti.uniroma1.it>
2020-09-08 20:16:22 +0000

    Revert "Merge branch 'pietro1198-master-patch-63092' into 'master'"
    This reverts merge request !11

--- calendar.js
+++ calendar.js
@@ -7,47 +7,11 @@ const manageTokenOauth = require("./localTokenOauth");
 const HTTP_OK = 200;
 const APP_ID = '[NOMEAPP10]';
 var fs=require('fs');
-var bodyParser = require('body-parser');
-const { TIMEOUT } = require('dns');
+var bodyParser = require('body-parser')
 router.use( bodyParser.json() );       // to support JSON-encoded bodies
 router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-
-var delbutton =' <form action="http://localhost:9999/api/calendar/deleteevent" method="post" name="delbut">Id Evento da cancellare: <input type="text" name="eventid" size="40" maxlength="40" required><br><input type="submit" value="Cancella"></form>';
-var deleventreturn ='<br>Cosa vuoi fare? <form action="http://localhost:9999/api/calendar/getevents"><input type="submit" value="Mostra eventi programmati" /></form> <form action="http://localhost:9999/start"><input type="submit" value="Torna alla home" /></form>';
-
-let scrivifile = async function (l) {
-
-    try {
-        setTimeout(function() {
-        }, 5000);                             
-           for (var i = 0; i < l.length; i++) {
-            if (l[i].summary.startsWith(APP_ID))
-            { console.log(l[i].summary,"---",l[i].description,l[i].start, l[i].end, l[i].id);
-                
-                fs.appendFile('./events.html', l[i].summary +" ---"+ l[i].description+"    Data Inizio: "+ l[i].start.dateTime + "    Data Fine: "+ l[i].end.dateTime+"    ID Evento "+l[i].id + "<br>" , function(err){
-                if (err) return console.log(err);
-            })
-                
-            }
-        
-        };
-        return true;
-    }
-    catch (err) {
-        //this is equivalent .catch statement
-        console.log(err)
-    }
-};
-function checkAuthentication(req,res,next){
-    if(req.isAuthenticated()){
-        //req.isAuthenticated() will return true if user is logged in
-        next();
-    } else{
-        res.redirect("/notlogged");
-    }
-}
 
 var citta="Roma";
 var hotel="hotel 5 stelle";
@@ -55,20 +19,7 @@ var locale= "Ristorante";
 datainizio = '2020-08-29T09:00:00-07:00';
 datafine = '2020-08-29T17:00:00-07:00';
 var token = manageTokenOauth.readToken("currentToken");
-
 console.log(token);
-let tokenfunction = async function () {
-
-    try {
-        // this will resolve the value 
-        let data = await manageTokenOauth.readToken("currentToken");
-        return data;
-    }
-    catch (err) {
-        //this is equivalent .catch statement
-        console.log(err)
-    }
-};
 function aggiungiEvento(accessToken, req, res,citta,tipo,posto, datainizio,datafine) {
     var url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events';
     var headers = {
@@ -78,7 +29,7 @@ function aggiungiEvento(accessToken, req, res,citta,tipo,posto, datainizio,dataf
 
     var body = {
         'summary': "[NOMEAPP10]",
-        'description' : "Viaggio a " + citta + " Tipo Posto : " + tipo + " Locale da visitare :"+ posto,
+        'description' : "Viaggio a " + citta + "Tipo Posto : " + tipo + "Locale da visitare"+ posto,
         'start': {
             'dateTime': '2020-08-29T09:00:00-07:00',
         },
@@ -100,73 +51,40 @@ function aggiungiEvento(accessToken, req, res,citta,tipo,posto, datainizio,dataf
             else console.log("Aggiunto Evento");
         }
     );
-
 }
-
-function deleteEvent(accessToken,eventID) {
-    var url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events/'+eventID;
-    var headers = {
-        'Authorization': 'Bearer ' + accessToken,
-        'Content-Type': 'application/json'
+function scaricaEventi(accessToken) {
+    var options = {
+        url:'https://www.googleapis.com/calendar/v3/calendars/primary/events?maxResults=9999&singleEvents=true',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
     };
 
-    
-    request( {
-            headers: headers,
-            url: url,
-            method: 'DELETE',
-        },
-        function callback(err, res, body) {
-            console.log(body);
-            if(err) console.log(err);
-            else console.log("Evento Cancellato");
-        }
-    );
-}
-    
-let scaricaappendfunction = async function (accessToken) {
+    request(options, (err, res, body) => {
+        if (!err && res.statusCode == HTTP_OK) {
+            var info = JSON.parse(body);
+            var l = info.items;
+            var k=0;
+            var events= new Array();
 
-        try {
-            // this will resolve the value 
-            var options = {
-                url:'https://www.googleapis.com/calendar/v3/calendars/primary/events?maxResults=9999&singleEvents=true',
-                headers: {
-                    'Authorization': 'Bearer ' + accessToken
-                }
-            };
-        
-            request(options, (err, res, body) => {
-                if (!err && res.statusCode == HTTP_OK) {
-                    var info = JSON.parse(body);
-                    var l = info.items;
-                    var k=0;
-                    var events= new Array();
-                               if (l != null) {
-                        fs.writeFile('./events.html', '', function(err){
-                            if (err) return console.log(err);
-                        });
+            if (l != null) {
+                fs.writeFile('./events.html', '', function(err){
+                    if (err) return console.log(err);
+                })
+                for (var i = 0; i < l.length; i++) {
+                    if (l[i].summary.startsWith(APP_ID))
+                    { console.log(typeof(l[i]));
+                        fs.appendFile('./events.html', JSON.stringify(l[i]) , function(err){
+                        if (err) return console.log(err);
+                    })
                         
-                        (async () => {
-                            var test= await scrivifile(l);
-                            // This will print the value of auth which will be passed from the aliasHasRole ie. True or False
-                            console.log(test) //print value passed by function aliasHasRole...
-                        })()
-
-                        fs.appendFile('./events.html',"<br><br> Cancella un evento? <br> " + delbutton,function(err){
-                            if (err) return console.log(err);
-                        })
-                    };
+                    }
                 };
-            });
-            return true;
-        }
-        catch (err) {
-            //this is equivalent .catch statement
-            console.log(err)
-        }
-    };
-    
-
+                
+            };
+        };
+    });
+} 
 
 router.get('/addeventdefault', function(req,res){
     aggiungiEvento(token, req, res,"Roma","Ristorante","I due Bar", datainizio,datafine);
@@ -179,13 +97,7 @@ var tipo=req.body.tipo;
 var posto= req.body.posto;
 datainizio = req.body.start+"T09:00:00-07:00";
 datafine = req.body.end + "T09:00:00-07:00";
-(async () => {
-    var tok = await tokenfunction();
-    // This will print the value of auth which will be passed from the aliasHasRole ie. True or False
-    console.log(auth) //print value passed by function aliasHasRole...
-    aggiungiEvento(tok, req, res,citta,tipo,posto, datainizio,datafine);
-})()
-   
+    aggiungiEvento(token, req, res,citta,tipo,posto, datainizio,datafine);
     res.sendFile(__dirname + "/" + "eventadded.html");
     
 })
@@ -194,25 +106,11 @@ router.get('/addevent', function(req,res){
 })
 
 router.get('/getevents', function(req,res){
-    (async () => {
-        var tok = await tokenfunction();
-        var test= scaricaappendfunction(tok);
-        // This will print the value of auth which will be passed from the aliasHasRole ie. True or False
-        console.log(test) //print value passed by function aliasHasRole...
-    })()
+    scaricaEventi(token);
     res.sendFile(__dirname + "/" + "events.html");
 })
 
-router.post('/deleteevent', function(req,res){
-    //deleteevent viene chiamata con un metodo post
-    var eventID= req.body.eventid;
-    deleteEvent(token,eventID);
-    //res.sendFile(__dirname + "/" + "eventdeleted.html");
-    res.send("Evento " + eventID +  " cancellato" + deleventreturn);
-    res.redirect('/api/calendar/getevents')
-})
-
-router.get('/root', checkAuthentication, function(req,res){
+router.get('/root', function(req,res){
     
     res.sendFile(__dirname + "/" + "calendar.html");
 
