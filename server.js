3ce2ffa9ac99ffe35b794ba018458ec43285cef7
unknown <sirico.1810153@studenti.uniroma1.it>
2020-09-08 22:24:19 +0200

    ok

--- server.js
+++ server.js
@@ -48,9 +48,9 @@ var session_id = receive.session_id;
 receive.ricevi();
 
 
-fs.writeFile('./cronologia.html', '<h1>CRONOLOGIA</h1>', function (err) {
+/*fs.writeFile('./cronologia.html', '<h1>CRONOLOGIA</h1>', function (err) {
     if (err) return console.log(err);
-})
+})*/
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
@@ -78,7 +78,7 @@ app.get('/info', function (req, res) {
 app.get('/cronologia', function (req, result) {
     //receive.ricevi();
     //res.sendFile('./cronologia.html',{root:__dirname});
-    var testo = 'CRONOLOGIA ' + session_id;
+    var testo = 'CRONOLOGIA ' //+ session_id;
     const pool = new Pool({
         user: 'postgres',
         host: 'localhost',
